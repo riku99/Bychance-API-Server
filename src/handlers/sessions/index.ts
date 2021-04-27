4f33cf8ec848fd21ff764dcead264eec4d03fcf4
riku <riku.suny@gmail.com>
2021-04-27 13:00:15 +0900

    lineログイン用エンドポイントにアクセス

--- src/handlers/sessions/index.ts
+++ src/handlers/sessions/index.ts
@@ -6,11 +6,21 @@ import { SessionsReqestType } from "~/routes/sessions/validator";
 const line = {
   create: async (req: Hapi.Request, h: Hapi.ResponseToolkit) => {
     const headers = req.headers as SessionsReqestType["line"]["create"]["headers"];
+    const token = headers.authorization.split(" ")[1]; // Bearer取り出し
+    const body = `id_token=${token}&client_id=${process.env.ChannelId}`; // x-www-form-urlencodedに形成
 
-    const token = headers.authorization.split(" ");
+    try {
+      const res = await axios.post(
+        "https://api.line.me/oauth2/v2.1/verify",
+        body,
+        { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+      );
 
-    console.log(token[1]);
-    console.log(headers);
+      console.log(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+
     return h.response().code(200); // payload返さない場合でもcode指定しないとエラーコード返る
   },
 };
