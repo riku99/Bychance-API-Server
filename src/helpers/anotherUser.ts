f53d52ae305b0f0ee4eb2d783f4ee5f5d2a16239
riku <riku.suny@gmail.com>
2021-05-04 14:48:56 +0900

    add createAnotherUser

---
+++ src/helpers/anotherUser.ts
@@ -0,0 +1,66 @@
+import { User, Post, Flash, ViewedFlash } from "@prisma/client";
+
+import { serializePost } from "~/serializers/post";
+import { serializeFlash } from "~/serializers/flash";
+import { dayMs } from "~/constants/date";
+
+type Arg = {
+  user: User;
+  posts: Post[];
+  flashes: Flash[];
+  viewedFlashes: ViewedFlash[]; // 返すユーザーではなく、自分(リクエストした側)のものを渡す
+};
+
+export const createAnotherUser = ({
+  user,
+  posts,
+  flashes,
+  viewedFlashes,
+}: Arg) => {
+  const { id, name, avatar, introduce, statusMessage } = user;
+
+  const serializedPosts = posts.map((post) => serializePost({ post }));
+
+  const viewedFlashIds = viewedFlashes.map((viewedFlash) => viewedFlash.id);
+
+  const alreadyViewedIds: number[] = [];
+
+  const notExpiredFlashes = flashes.filter((flash) => {
+    if (viewedFlashIds.includes(flash.id)) {
+      alreadyViewedIds.push(flash.id);
+    }
+
+    return (
+      // 作成してから2日以内の物を取り出す
+      // あとでcreateClientDataのものとまとめる
+      // DBから取り出す段階で指定できそう
+      (new Date().getTime() - new Date(flash.createdAt).getTime()) / dayMs < 2
+    );
+  });
+
+  const lastId =
+    notExpiredFlashes.length &&
+    notExpiredFlashes[notExpiredFlashes.length - 1].id;
+
+  const isAllAlreadyViewed = alreadyViewedIds.includes(lastId) ? lastId : false;
+
+  const serializedFlashes = notExpiredFlashes.map((flash) =>
+    serializeFlash({ flash })
+  );
+
+  const flashesData = {
+    entities: serializedFlashes,
+    alreadyViewed: alreadyViewedIds,
+    isAllAlreadyViewed,
+  };
+
+  return {
+    id,
+    name,
+    avatar,
+    introduce,
+    statusMessage,
+    posts: serializedPosts,
+    flashes: flashesData,
+  };
+};
