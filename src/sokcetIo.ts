8f1b8e9fc9b409367d14f826781d1f2acb54312d
riku <riku.suny@gmail.com>
2021-05-13 12:23:14 +0900

    socket切断の対応

--- src/sokcetIo.ts
+++ src/sokcetIo.ts
@@ -2,6 +2,14 @@ import { talkRoomMessageNameSpace } from "~/server";
 
 export const setupSocketIo = () => {
   talkRoomMessageNameSpace.on("connection", async (socket) => {
-    await socket.join(socket.handshake.query.id as string); // ユーザーのidでソケット通信ができるようにjoinしてroomを作成
+    const query = socket.handshake.query;
+    if (!query) {
+      socket.disconnect();
+      return;
+    }
+    await socket.join(query.id as string); // ユーザーのidでソケット通信ができるようにjoinしてroomを作成
+    socket.on("disconnect", async () => {
+      socket.disconnect();
+    });
   });
 };
