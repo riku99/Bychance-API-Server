a7a41ebb034d5bd437cf2917ae2298658c1e98b7
riku <riku.suny@gmail.com>
2021-05-15 18:19:49 +0900

    socketの修正

--- src/sokcetIo.ts
+++ src/sokcetIo.ts
@@ -4,12 +4,11 @@ export const setupSocketIo = () => {
   talkRoomMessageNameSpace.on("connection", async (socket) => {
     const query = socket.handshake.query;
     if (!query) {
-      socket.disconnect();
       return;
     }
     await socket.join(query.id as string); // ユーザーのidでソケット通信ができるようにjoinしてroomを作成
-    socket.on("disconnect", async () => {
-      socket.disconnect();
-    });
+
+    // clientが明示的にdisconnectした場合(ログアウトとか)はサーバー側で何かしなくてもsocketは切断される。なのでとりあえずサーバ側からは何もしない
+    //socket.on("disconnect", async () => {});
   });
 };
