583cae9d9b6305146d007897cf199027e755f92c
riku <riku.suny@gmail.com>
2021-08-14 19:29:42 +0900

    いらないディレクトリ削除

--- src/plugins/deleteTalkRooms/index.ts
+++
@@ -1,8 +0,0 @@
-import Hapi from "@hapi/hapi";
-
-import { deleteTalkRoomsRoute } from "~/routes/deleteTalkRooms";
-
-export const deleteTalkRoomsPlugin: Hapi.Plugin<undefined> = {
-  name: "app/routes/deleteTalkRooms",
-  register: deleteTalkRoomsRoute,
-};
