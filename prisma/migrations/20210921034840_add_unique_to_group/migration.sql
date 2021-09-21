74e6b16ae51275a6a02d253f3a9178eff0a7ce52
riku <riku.suny@gmail.com>
2021-09-21 13:01:45 +0900

    fix groups handler

---
+++ prisma/migrations/20210921034840_add_unique_to_group/migration.sql
@@ -0,0 +1,2 @@
+-- AlterIndex
+ALTER TABLE `Group` RENAME INDEX `Group_ownerId_unique` TO `Group.ownerId_unique`;
--- src/handlers/groups.ts
+++ src/handlers/groups.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 import { CreateGroupsPayload } from "~/routes/groups/validators";
 import { Artifacts } from "~/auth/bearer";
+import { throwInvalidError } from "~/helpers/errors";
 
 const prisma = new PrismaClient();
 
@@ -10,21 +11,39 @@ const create = async (req: Hapi.Request, h: Hapi.ResponseToolkit) => {
   const user = req.auth.artifacts as Artifacts;
   const payload = req.payload as CreateGroupsPayload;
 
+  // 他のグループに所属状態である場合は新しいグループに入ることはできない
+  if (user.groupId) {
+    return throwInvalidError(
+      "既にグループに所属しています。新しいグループに入るためには抜けてください。",
+      true
+    );
+  }
+
   const newGroup = await prisma.group.create({
     data: {
       ownerId: payload.ownerId,
     },
   });
 
-  await prisma.user.update({
+  // 申請していた側も申請されていた側もをグループに所属させる
+  await prisma.user.updateMany({
     where: {
-      id: user.id,
+      OR: [
+        {
+          id: user.id,
+        },
+        {
+          id: payload.ownerId,
+          groupId: null,
+        },
+      ],
     },
     data: {
       groupId: newGroup.id,
     },
   });
 
+  // グループに所属したら申請された側の申請中のものはすべて消去
   await prisma.applyingGroup.deleteMany({
     where: {
       applyingUserId: user.id,
