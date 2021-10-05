4ca2cf54968b86528be64d65da6483e77acf395d
riku <riku.suny@gmail.com>
2021-10-06 01:21:17 +0900

    add posts test

--- tests/apis/posts.test.ts
+++ tests/apis/posts.test.ts
@@ -95,4 +95,167 @@ describe("posts", () => {
       expect(res.statusCode).toEqual(401);
     });
   });
+
+  describe("Postの削除, DELETE /posts/{postId}", () => {
+    const url = `${baseUrl}/posts`;
+
+    test("認可情報がない場合、401エラーが返される", async () => {
+      const res = await server.inject({
+        method: "DELETE",
+        url: `${url}/1`,
+      });
+
+      expect(res.statusCode).toEqual(401);
+    });
+
+    test("認可情報が間違っている場合、401エラーが返される", async () => {
+      const user = await prisma.user.create({
+        data: {
+          name: "name",
+          lineId: "lineId",
+          accessToken: hashedAccessToken,
+        },
+      });
+
+      const res = await server.inject({
+        method: "DELETE",
+        url: `${url}/1?id=${user.id}`,
+        headers: { Authorization: "BearerWrongBearer" },
+      });
+
+      expect(res.statusCode).toEqual(401);
+    });
+
+    test("Postが削除される", async () => {
+      const user = await prisma.user.create({
+        data: {
+          name: "name",
+          lineId: "lineId",
+          accessToken: "tokne",
+        },
+      });
+
+      const post = await prisma.post.create({
+        data: {
+          url: "url",
+          userId: user.id,
+        },
+      });
+
+      await server.inject({
+        method: "DELETE",
+        url: `${url}/${post.id}`,
+        auth: {
+          strategy: "simple",
+          credentials: {},
+          artifacts: user,
+        },
+      });
+
+      const existedPost = await prisma.post.findUnique({
+        where: {
+          id: post.id,
+        },
+      });
+
+      expect(existedPost).toBeNull();
+    });
+
+    test("選択したPostのuserIdとリクエストユーザーのidが異なる場合、400エラーが返される", async () => {
+      const user = await prisma.user.create({
+        data: {
+          name: "name",
+          lineId: "lineId",
+          accessToken: "tokne",
+        },
+      });
+
+      const post = await prisma.post.create({
+        data: {
+          url: "url",
+          userId: user.id,
+        },
+      });
+
+      const res = await server.inject({
+        method: "DELETE",
+        url: `${url}/${post.id}`,
+        auth: {
+          strategy: "simple",
+          credentials: {},
+          artifacts: {
+            id: "abcde" + user.id, // リクエストユーザーのIdがpostのuserIdと違う
+          },
+        },
+      });
+
+      expect(res.statusCode).toEqual(400);
+    });
+  });
+
+  describe("Postの取得, GET /users/{userId}/posts", () => {
+    test("認可情報がない場合、401エラーを返す", async () => {
+      const res = await server.inject({
+        method: "GET",
+        url: `${baseUrl}/users/1/posts`,
+      });
+
+      expect(res.statusCode).toEqual(401);
+    });
+
+    test("認可情報が間違っている場合、401エラーを返す", async () => {
+      const user = await prisma.user.create({
+        data: {
+          name: "name",
+          lineId: "lineId",
+          accessToken: hashedAccessToken,
+        },
+      });
+
+      const res = await server.inject({
+        method: "GET",
+        url: `${baseUrl}/users/1/posts?id=${user.id}`,
+        headers: { Authorization: "BearerWrongBearer" },
+      });
+
+      expect(res.statusCode).toEqual(401);
+    });
+
+    test("指定したユーザーのPostを全て取得する", async () => {
+      const user = await prisma.user.create({
+        data: {
+          name: "name",
+          lineId: "lineId",
+          accessToken: "accessToken",
+        },
+      });
+
+      const post1 = await prisma.post.create({
+        data: {
+          url: "url",
+          userId: user.id,
+        },
+      });
+
+      const post2 = await prisma.post.create({
+        data: {
+          url: "url",
+          userId: user.id,
+        },
+      });
+
+      const res = await server.inject({
+        method: "GET",
+        url: `${baseUrl}/users/${user.id}/posts`,
+        auth: {
+          strategy: "simple",
+          credentials: {},
+          artifacts: user,
+        },
+      });
+
+      console.log(res.payload);
+      expect(JSON.parse(res.payload).length).toEqual(2);
+    });
+  });
 });
